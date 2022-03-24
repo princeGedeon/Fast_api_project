914ccc7e3298d81abd3a043d49de0e918e753c77
prince9603 <guedjegedeon03@gmail.com>
2022-03-24 19:27:18 +0100

    Montage d'un TodoApp

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-from typing import Optional
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-app=FastAPI()
-class Coord(BaseModel):
-    x:float
-    y:float
-    zoom:Optional[int]
+app=FastAPI(title="TODO List | By Prince",v1=1.0)
 
-@app.post("/position")
-async def make_position(Coord:Coord):
-    return {"new":Coord}
-
+class Todo(BaseModel):
+    name:str
+    due_date:str
 @app.get("/")
 async def hello():
     return {"hello":"world"}
-
-@app.get("/produit/{product_id}")
-async def read_product(product_id:int,nom:Optional[str]=None):
-    return {"id":product_id,"nom":nom}
